4d8aaac6b3ba484cebc61097a4d1022378f814c3
diegofch29 <diegofch29@hotmail.com>
2018-04-23 11:05:55 -0500

    claseProfesor creada -falta/ mapper,batis,dao en proceso

---
+++ src/main/java/edu/eci/pdsw/samples/entities/Profesor.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.samples.entities;
+
+/**
+ *
+ * @author diego
+ */
+public class Profesor {
+    private long id;
+    private String nombre;
+    private String apellido;
+    //private Cursos[] grupos;
+    private String correo;
+    private String telefono;
+    
+    public Profesor(){
+    }
+    
+    public Profesor(long id,String nombre,String apellido,String correo,String telefono){
+        this.id=id;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.correo=correo;
+        this.telefono=telefono;
+    }
+    
+    public long getId(){
+        return id;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getApellido(){
+        return apellido;
+    }
+    
+    public String getCorreo(){
+        return correo;
+    }
+    
+    public String getTelefono(){
+        return telefono;
+    } 
+    
+    public void setId(long id){
+        this.id=id;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public void setApellido(String apellido){
+        this.apellido=apellido;
+    }
+    
+    public void setCorreo(String correo){
+        this.correo=correo;
+    }
+    
+    public void setTelefono(String telefono){
+        this.telefono=telefono;
+    }
+    
+}
